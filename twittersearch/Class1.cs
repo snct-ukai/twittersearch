e49cbd948d88ef6a57ba473d8db8c6451d07b187
snct-ukai <73654679+snct-ukai@users.noreply.github.com>
2021-02-26 04:22:25 +0900

    プロジェクト ファイルを追加します。

---
+++ twittersearch/Class1.cs
@@ -0,0 +1,29 @@
+﻿namespace twittersearch
+{
+    public class APIKeys
+    {
+        public static string Apikey()
+        {
+            const string key = "API Key";
+            return key;
+        }
+
+        public static string Apikeysecret()
+        {
+            const string key = "API Key secret";
+            return key;
+        }
+
+        public static string Accesstoken()
+        {
+            const string key = "Access token";
+            return key;
+        }
+
+        public static string Accesstokensecret()
+        {
+            const string key = "Access token secret";
+            return key;
+        }
+    }
+}
---
+++ twittersearch/MainActivity.cs
@@ -0,0 +1,52 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace twittersearch
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        public TextView Result { get; private set; }
+        public TextView searchword { get; private set; }
+
+        public Button Search { get; private set; }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Result = FindViewById<TextView>(Resource.Id.text);
+            searchword = FindViewById<EditText>(Resource.Id.searchword);
+
+            Search = FindViewById<Button>(Resource.Id.search);
+            Search.Click += Search_Click;
+        }
+
+        private async void Search_Click(object sender, System.EventArgs e)
+        {
+            var token = CoreTweet.Tokens.Create(APIKeys.Apikey(), APIKeys.Apikeysecret(), APIKeys.Accesstoken(), APIKeys.Accesstokensecret());
+            var search = searchword.Text;
+            var result = await token.Search.TweetsAsync(count => 5, q => search);
+
+            Result.Text = search + "\n";
+
+            foreach (var tweet in result)
+            {
+                Result.Text += "\n---------------\n" + tweet.User.ScreenName + "\n\n" + tweet.Text;
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
